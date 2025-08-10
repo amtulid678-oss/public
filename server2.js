9f2b84278be4168e73c45f93c4d7a1057a8e9ac5
amtulid678-oss <amtulid678@gmail.com>
2025-08-10 20:18:21 +0545

    minor changes

--- server2.js
+++ server2.js
@@ -1,8 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import express from "express";
 import multer from "multer";
-import fs from "fs";
-import path from "path";
 
 const app = express();
 const PORT = 3000;
